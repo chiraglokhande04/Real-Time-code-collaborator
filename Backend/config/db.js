cc924d421c4ebab0a5ee8e38f25ff79854a97546
chiraglokhande04 <chiraglokhande14@gmail.com>
2025-04-27 19:18:43 +0530

    pushing

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect('mongodb+srv://chiraglokhande04:mdci1234@cluster0.au19e.mongodb.net/codeCollab');
         console.log('Connected to MongoDB...');
     } catch (err) {
         console.error('Could not connect to MongoDB...', err);
