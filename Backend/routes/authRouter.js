6ac33eafdf39e7fb14514e38072e151e5c9c7546
chiraglokhande04 <chiraglokhande14@gmail.com>
2025-05-31 21:23:04 +0530

    deploy frontend

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -8,8 +8,8 @@ authRouter.get('/google',passport.authenticate('google',{scope:['profile','email
 
 authRouter.get("/google/callback",
     passport.authenticate("google", {
-        successRedirect: "http://localhost:3001/dashboard",
-        failureRedirect: "http://localhost:3001/login"
+        successRedirect: "https://real-time-code-collaborator-two.vercel.app/logged",
+        failureRedirect: "https://real-time-code-collaborator-two.vercel.app"
     })
 );
 
